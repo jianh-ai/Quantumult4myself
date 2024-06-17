94bad62746b6019b2f30a1ec10357386c1958d6a
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-06-17 21:30:35 +0800

    Update ali.js

--- js/ali.js
+++ js/ali.js
@@ -21,56 +21,6 @@ if (url.includes("/amdc/mobileDispatch")) {
   } else {
     $done({});
   }
-} else if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
-  // 12306页面内容
-  const list12306 = [
-    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
-    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
-    // "com.cars.otsmobile.city",
-    // "com.cars.otsmobile.initCountry",
-    // "com.cars.otsmobile.initNewSysCache",
-    // "com.cars.otsmobile.initProvince",
-    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
-    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
-    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
-    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
-    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
-    "com.cars.otsmobile.newHomePageBussData" // 商品信息流
-    // "com.cars.otsmobile.newHomePageRefresh",
-    // "com.cars.otsmobile.travelPage.initData", // 出行服务
-  ];
-  if (isQuanX) {
-    if (list12306?.includes(headopt)) {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done({});
-    }
-  } else {
-    if (list12306?.includes(headopt)) {
-      $done();
-    } else {
-      $done({});
-    }
-  }
-} else if (url.includes("/mobilepaas.abchina.com.cn:441/mgw")) {
-  // 中国农业银行开屏广告
-  const listbankabc = [
-    "com.bankabc.recommendcenter.homepage.gethpadverinfo",
-    "com.abchina.mbank.common.homepage.getStartParam"
-  ];
-  if (isQuanX) {
-    if (listbankabc?.includes(headopt)) {
-      $done({ status: "HTTP/1.1 404 Not Found" });
-    } else {
-      $done({});
-    }
-  } else {
-    if (listbankabc?.includes(headopt)) {
-      $done();
-    } else {
-      $done({});
-    }
-  }
-} else {
+}else {
   $done({});
 }
