b5000c20b402d61e09ef5eea3b3abb98b483b31d
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-06-17 21:32:25 +0800

    Update ali.js

--- js/ali.js
+++ js/ali.js
@@ -6,12 +6,12 @@ const isQuanX = typeof $task !== "undefined";
 
 if (url.includes("/amdc/mobileDispatch")) {
   if (
-       ua.includes("AMapiPhone") // 高德地图
-    || ua.includes("Alibaba") // 阿里巴巴
-    || ua.includes("Taobao4iPhone") // 淘宝
-    || ua.includes("TmallCampus") //天猫校园
-    || ua.includes("Cainiao4iPhone") // 菜鸟
-    || ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
+    ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Alibaba") || // 阿里巴巴
+    ua.includes("Taobao4iPhone") || // 淘宝
+    ua.includes("TmallCampus") || //天猫校园
+    ua.includes("Cainiao4iPhone") || // 菜鸟
+    ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
   ) {
     if (isQuanX) {
       $done({ status: "HTTP/1.1 404 Not Found" });
