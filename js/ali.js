122f06d6755124d37bbf9431fe5259d135498019
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-06-29 12:24:57 +0800

    Update ali.js

--- js/ali.js
+++ js/ali.js
@@ -8,7 +8,7 @@ if (url.includes("/amdc/mobileDispatch")) {
   if (
     ua.includes("AMapiPhone") || // 高德地图
     ua.includes("Alibaba") || // 阿里巴巴
-    ua.includes("%E6%B7%98%E5%AE%9D") || // 淘宝
+    //ua.includes("%E6%B7%98%E5%AE%9D") || // 淘宝
     ua.includes("TmallCampus") || //天猫校园
     ua.includes("Cainiao4iPhone") || // 菜鸟
     ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
