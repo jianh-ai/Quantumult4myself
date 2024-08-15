a9e8b5df9d83e1b48a9face6faceebe4535eab1a
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-08-15 17:44:26 +0800

    Update missevan.js

--- js/missevan.js
+++ js/missevan.js
@@ -4,38 +4,35 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/site/launch")) {
-    if (obj.info) delete obj.info;
+    delete obj.info;
 }
 
 if (url.includes("/v2/meta/banner")) {
-    if (obj.info) delete obj.info;
+    delete obj.info;
 }
 
 if (url.includes("/site/get-top")) {
-    if (obj.info && obj.info.extra_banners) 
-        delete obj.info.extra_banners;
+    delete obj.info.extra_banners;
 }
 
 if (url.includes("/api/v2/recommended/top")) {
-    if (obj.info) delete obj.info;
+    delete obj.info;
 }
 
 if (url.includes("/drama/catalog-homepage")) {
-    if (obj.info && obj.info.extra_banners) 
-        delete obj.info.extra_banners;
+    delete obj.info.extra_banners;
 }
 
 if (url.includes("/catalog/sound-homepage")) {
-    if (obj.info && obj.info.extra_banners) 
         delete obj.info.extra_banners;
 }
 
 if (url.includes("/api/v2/chatroom/sound/recommend")) {
-    if (obj.info) delete obj.info;
+    delete obj.info;
 }
 
 if (url.includes("/site/config")) {
-    if (obj.info && obj.info.teenager_popup_mode) delete obj.info.teenager_popup_mode;
+    delete obj.info.teenager_popup_mode;
 }
 
 $done({body: JSON.stringify(obj)});
