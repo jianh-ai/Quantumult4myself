16420b5bbb2b83c6e564d16937b268035979fe59
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-08-15 12:56:09 +0800

    Update missevan.js

--- js/missevan.js
+++ js/missevan.js
@@ -39,14 +39,12 @@ if (url.includes("/site/icons")) {
     if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
         obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
     }
-    $done({ body: JSON.stringify(obj) });
 } else (url.includes("/discovery/list")) {
     for (const index in obj.info) {
         obj.info[index] = obj.info[index].filter(item => {
           return !(item.title === '直播间' || item.title === '广播剧' || item.title === '免流服务')
         })
     }
-    $done({body: JSON.stringify(obj)})
 } 
 
 $done({body: JSON.stringify(obj)});
