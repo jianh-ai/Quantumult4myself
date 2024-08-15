733ebbe80d3075c9e1913d859b1559bcd23e89c8
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-08-15 17:38:17 +0800

    Update missevan.js

--- js/missevan.js
+++ js/missevan.js
@@ -4,48 +4,53 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (url.includes("/site/launch")) {
-    delete obj.info;
+    if (obj.info) delete obj.info;
 }
 
 if (url.includes("/v2/meta/banner")) {
-    delete obj.info;
+    if (obj.info) delete obj.info;
 }
 
 if (url.includes("/site/get-top")) {
-    delete obj.info.extra_banners;
+    if (obj.info && obj.info.extra_banners) 
+        delete obj.info.extra_banners;
 }
 
 if (url.includes("/api/v2/recommended/top")) {
-    delete obj.info;
+    if (obj.info) delete obj.info;
 }
 
 if (url.includes("/drama/catalog-homepage")) {
-    delete obj.info.extra_banners;
+    if (obj.info && obj.info.extra_banners) 
+        delete obj.info.extra_banners;
 }
 
 if (url.includes("/catalog/sound-homepage")) {
-    delete obj.info.extra_banners;
+    if (obj.info && obj.info.extra_banners) 
+        delete obj.info.extra_banners;
 }
 
 if (url.includes("/api/v2/chatroom/sound/recommend")) {
-    delete obj.info;
+    if (obj.info) delete obj.info;
 }
 
 if (url.includes("/site/config")) {
-    delete obj.info.teenager_popup_mode;
+    if (obj.info && obj.info.teenager_popup_mode) delete obj.info.teenager_popup_mode;
 }
 
-if (url.includes("/site/icons")) {
+if (url.includes("/site/icons")) 
     if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
         obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
     }
 }
 
 if (url.includes("/discovery/list")) {
-    for (const index in obj.info) {
-        obj.info[index] = obj.info[index].filter(item => {
-            return !(item.title === '直播间' || item.title === '广播剧' || item.title === '免流服务');
-        });
+    if (obj.info) {
+        for (const index in obj.info) {
+            obj.info[index] = obj.info[index].filter(item => {
+                return !(item.title === '直播间' || item.title === '广播剧' || item.title === '免流服务');
+            });
+        }
     }
 }
 
