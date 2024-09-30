7eac919b39d2bbce7b3f32cb9927e69abc138b2b
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-09-30 21:21:13 +0800

    Update missevan.js

--- js/missevan.js
+++ js/missevan.js
@@ -3,6 +3,20 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
+if (url.includes("/site/icons")) {
+    if (obj.info && obj.info.icons && Array.isArray(obj.info.icons)) {
+        obj.info.icons = obj.info.icons.filter(icon => icon.title !== "直播" && icon.title !== "周边商城");
+    }
+} 
+
+if (url.includes("/discovery/list")) {
+    for (const index in obj.info) {
+        obj.info[index] = obj.info[index].filter(item => {
+          return !(item.title === '直播间' || item.title === '广播剧' || item.title === '免流服务')
+        })
+    }
+} 
+
 // 首页 - 直播 - 热门 - 主播横幅推广
 if (url.includes("/api/v2/meta/banner")) {
   delete obj.info;
