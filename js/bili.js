1f90c1169454440f7bcf1063d6bb38b907192e6f
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2025-03-30 13:27:49 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -1,214 +1,218 @@
-// 2024-10
+// 2024-05-10
 
-const url = $request.url;
-if (!$response.body) $done({});
-let obj = JSON.parse($response.body);
+let body = $response.body;
 
-if (url.includes("/x/resource/show/skin")) {
-  // 皮肤推送
-  if (obj?.data?.common_equip) {
-    delete obj.data.common_equip;
-  }
-} else if (url.includes("/x/resource/show/tab/v2")) {
-  // 底部选项卡
-  if (obj?.data?.bottom?.length > 0) {
-    const sortLists = ["首页", "动态", "我的"];
-    obj.data.bottom = obj.data.bottom
-      .filter((i) => sortLists?.includes(i?.name))
-      .sort((a, b) => sortLists.indexOf(a?.name) - sortLists.indexOf(b?.name));
-  }
-  // 首页导航栏
-  if (obj?.data?.tab?.length > 0) {
-    const sortLists = ["直播", "推荐", "热门"];
-    obj.data.tab = obj.data.tab
-      .filter((i) => sortLists?.includes(i?.name))
-      .sort((a, b) => sortLists.indexOf(a?.name) - sortLists.indexOf(b?.name));
-  }
-  // 右上角按钮
-  if (obj?.data?.top?.length > 0) {
-    obj.data.top = obj.data.top.filter((i) => i?.name === "消息");
-    if (obj?.data?.top?.[0]?.pos) {
-      obj.data.top[0].pos = 1;
-    }
-  }
-} else if (url.includes("/x/resource/top/activity")) {
-  // 首页右上角活动
-  obj = { code: -404, message: "啥都木有", ttl: 1, data: null };
-} else if (url.includes("/x/v2/account/mine")) {
-  // 我的页面
-  const del = ["ipad_upper_sections", "rework_v1", "vip_section", "vip_section_v2"];
-  for (let i of del) {
-    delete obj.data[i]; // 不必要项目
-  }
-  // iPad 我的页面
-  if (obj?.data?.ipad_recommend_sections?.length > 0) {
-    const itemList = [789, 790]; // 789我的关注 790我的消息 791我的钱包 792直播中心 793大会员 794我的课程 2542我的游戏
-    obj.data.ipad_recommend_sections = obj.data.ipad_recommend_sections.filter((i) => itemList?.includes(i.id));
-  }
-  if (obj?.data?.ipad_more_sections?.length > 0) {
-    const itemList = [797, 798]; // 797我的客服 798设置 1070青少年守护
-    obj.data.ipad_more_sections = obj.data.ipad_more_sections.filter((i) => itemList?.includes(i.id));
-  }
-  // iPhone 我的页面
-  if (obj?.data?.sections_v2?.length > 0) {
-    let newSects = [];
-    for (let item of obj.data.sections_v2) {
-      if (item?.button) {
-        delete item.button;
-      }
-      if (item?.style) {
-        if (item?.style === 1 || item?.style === 2) {
-          if (item?.title) {
-            if (item?.title === "创作中心" || item?.title === "推荐服务") {
-              continue; // 创作中心 推荐服务
-            } else if (item?.title === "更多服务") {
-              if (item?.title) {
-                delete item.title;
-              }
-              if (item?.items?.length > 0) {
-                let newItems = [];
-                for (let i of item.items) {
-                  if (/user_center\/feedback/g.test(i?.uri)) {
-                    newItems.push(i); // 联系客服
-                  } else if (/user_center\/setting/g.test(i?.uri)) {
-                    newItems.push(i); // 设置
-                  } else {
-                    continue;
-                  }
-                }
-                item.items = newItems;
-              }
-            }
-          }
-        } else {
-          continue; // 其他style
-        }
-      }
-      newSects.push(item);
-    }
-    obj.data.sections_v2 = newSects;
-  }
-  // 非会员开启本地会员标识
-  if (obj?.data?.vip) {
-    if (obj?.data?.vip?.status === 0) {
-      obj.data.vip_type = 2;
-      obj.data.vip.type = 2;
-      obj.data.vip.status = 1;
-      obj.data.vip.due_date = 3818419199; // Unix 时间戳 2090-12-31 23:59:59
-      obj.data.vip.label = {
-        path: "",
-        text: "年度大会员",
-        label_theme: "annual_vip",
-        text_color: "#FFFFFF",
-        bg_style: 1,
-        bg_color: "#FB7299",
-        border_color: "",
-        image: "https://i0.hdslb.com/bfs/vip/8d4f8bfc713826a5412a0a27eaaac4d6b9ede1d9.png"
-      };
-      obj.data.vip.nickname_color = "#FB7299";
-      obj.data.vip.role = 3;
-    }
-  }
-} else if (url.includes("/x/v2/account/myinfo")) {
-  // 非会员开启会员专属清晰度
-  if (obj?.data?.vip) {
-    if (obj?.data?.vip?.status === 0) {
-      obj.data.vip.type = 2;
-      obj.data.vip.status = 1;
-      obj.data.vip.due_date = 3818419199; // Unix 时间戳 2090-12-31 23:59:59
-      obj.data.vip.role = 3;
-    }
-  }
-} else if (url.includes("/x/v2/feed/index?")) {
-  // 首页推荐信息流
-  if (obj?.data?.items?.length > 0) {
-    // 白名单
-    obj.data.items = obj.data.items.filter((i) => i?.card_goto === "av");
-  }
-} else if (url.includes("/x/v2/feed/index/story")) {
-  // 竖屏模式信息流
-  if (obj?.data?.items?.length > 0) {
-    // vertical_live 直播内容
-    // vertical_pgc 大会员专享
-    let newItems = [];
-    for (let item of obj.data.items) {
-      if (item?.hasOwnProperty("ad_info")) {
-        continue;
-      } else if (["vertical_ad_av", "vertical_live", "vertical_pgc"]?.includes(item?.card_goto)) {
-        continue;
-      } else {
-        if (item?.story_cart_icon) {
-          delete item.story_cart_icon;
-        }
-        newItems.push(item);
-      }
-    }
-    obj.data.items = newItems;
-  }
-} else if (url.includes("/x/v2/search/square")) {
-  // 搜索框
-  if (obj?.data) {
-    obj.data = { type: "history", title: "搜索历史", search_hotword_revision: 2 };
-  }
-} else if (url.includes("/x/v2/splash")) {
-  // 开屏广告
-  if (obj?.data) {
-    const item = ["account", "event_list", "preload", "show"];
-    item.forEach((i) => {
-      delete obj.data[i];
-    });
-    if (obj?.data?.max_time) {
-      obj.data.max_time = 0;
-    }
-    if (obj?.data?.min_interval) {
-      obj.data.min_interval = 31536000;
-    }
-    if (obj?.data?.pull_interval) {
-      obj.data.pull_interval = 31536000;
-    }
-    if (obj?.data?.list?.length > 0) {
-      for (let i of obj.data.list) {
-        i.duration = 0;
-        i.enable_pre_download = false;
-        i.begin_time = 3818332800; // Unix 时间戳 2090-12-31 00:00:00
-        i.end_time = 3818419199; // Unix 时间戳 2090-12-31 23:59:59
-      }
-    }
-  }
-} else if (url.includes("/pgc/page/bangumi") || url.includes("/pgc/page/cinema/tab")) {
-  // 观影页
-  if (obj.result?.modules?.length > 0) {
-    obj.result.modules.forEach((i) => {
-      if (i?.style?.startsWith("banner")) {
-        if (i?.items?.length > 0) {
-          i.items = i.items.filter((ii) => ii?.link?.includes("play"));
-        }
-      } else if (i?.style?.startsWith("function")) {
-        if (i?.items?.length > 0) {
-          i.items = i.items.filter((ii) => ii?.blink?.startsWith("bilibili"));
-        }
-      } else if ([241, 1283, 1284, 1441]?.includes(i?.module_id)) {
-        if (i?.items?.length > 0) {
-          i.items = [];
-        }
-      } else if (i?.style?.startsWith("tip")) {
-        if (i?.items?.length > 0) {
-          i.items = [];
-        }
-      }
-    });
-  }
-} else if (url.includes("/xlive/app-room/v1/index/getInfoByRoom")) {
-  // 直播
-  if (obj?.data?.activity_banner_info) {
-    delete obj.data.activity_banner_info;
-  }
-  if (obj?.data?.shopping_info) {
-    obj.data.shopping_info = { is_show: 0 };
-  }
-  if (obj?.data?.new_tab_info?.outer_list?.length > 0) {
-    obj.data.new_tab_info.outer_list = obj.data.new_tab_info.outer_list.filter((i) => i?.biz_id !== 33);
-  }
-}
-
-$done({ body: JSON.stringify(obj) });
+if (body) {
+	switch (!0) {
+		case /pgc\/season\/app\/related\/recommend\?/.test($request.url):
+			try {
+				let t = JSON.parse(body);
+				t.result?.cards?.length && (t.result.cards = t.result.cards.filter(t => 2 != t.type)), body = JSON.stringify(t)
+			} catch (i) {
+				console.log("bilibili recommend:" + i)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/resource\/show\/skin\?/.test($request.url):
+			try {
+				let a = JSON.parse(body);
+				delete a.data?.common_equip, body = JSON.stringify(a)
+			} catch (e) {
+				console.log("bilibili skin:" + e)
+			}
+			break;
+		case /^https:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\?/.test($request.url):
+			try {
+				let s = JSON.parse(body),
+					l = [];
+				for (let o of s.data.items)
+					if (!o.hasOwnProperty("banner_item")) {
+						if (!(!o.hasOwnProperty("ad_info") && -1 === o.card_goto?.indexOf("ad") && ["small_cover_v2", "large_cover_v1", "large_cover_single_v9"].includes(o.card_type))) continue;
+						else l.push(o)
+					} s.data.items = l, body = JSON.stringify(s)
+			} catch (d) {
+				console.log("bilibili index:" + d)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/v2\/feed\/index\/story\?/.test($request.url):
+			try {
+				let r = JSON.parse(body),
+					b = [];
+				for (let p of r.data.items) p.hasOwnProperty("ad_info") || -1 !== p.card_goto.indexOf("ad") || b.push(p);
+				r.data.items = b, body = JSON.stringify(r)
+			} catch (c) {
+				console.log("bilibili Story:" + c)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/v\d\/account\/teenagers\/status\?/.test($request.url):
+			try {
+				let n = JSON.parse(body);
+				n.data.teenagers_status = 0, body = JSON.stringify(n)
+			} catch (y) {
+				console.log("bilibili teenagers:" + y)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/resource\/show\/tab/.test($request.url):
+			try {
+				let u = new Set([177, 178, 179, 181, 102, 104, 106, 486, 488, 489]),
+					h = JSON.parse(body);
+				if (h.data?.tab) {
+					var f = [];
+					f.push({
+						id: 39,
+						name: "直播",
+						uri: "bilibili://live/home",
+						tab_id: "直播tab",
+						pos: 1
+					}, {
+						id: 40,
+						name: "推荐",
+						uri: "bilibili://pegasus/promo",
+						tab_id: "推荐tab",
+						pos: 2,
+						default_selected: 1
+					}), 
+                    
+                    f.push({
+						id: 41,
+						name: "热门",
+						uri: "bilibili://pegasus/hottopic",
+						tab_id: "hottopic",
+						pos: 3
+					}), 
+                    
+                    f.push({
+						id: 545,
+						name: "番剧",
+						uri: "bilibili://pgc/home",
+						tab_id: "bangumi",
+						pos: 4
+					}), 
+                    
+                    h.data.tab = f
+				}
+                
+				if (h.data.top && (h.data.top = [{
+						id: 481,
+						icon: "http://i0.hdslb.com/bfs/archive/d43047538e72c9ed8fd8e4e34415fbe3a4f632cb.png",
+						name: "消息",
+						uri: "bilibili://link/im_home",
+						tab_id: "消息Top",
+						pos: 1
+					}]), h.data.bottom) {
+					let m = h.data.bottom.filter(t => u.has(t.id));
+					h.data.bottom = m
+				}
+				body = JSON.stringify(h)
+			} catch (g) {
+				console.log("bilibili tabprocess:" + g)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/v2\/account\/mine/.test($request.url):
+			try {
+				let v = JSON.parse(body);
+				// 删除不必要项目
+				const del = ["ipad_upper_sections", "rework_v1"];
+				for (let i of del) {
+					if (v.data.hasOwnProperty(i)) {
+						delete v.data[i];
+					}
+				}
+	
+				// 需要保留的特定 ID
+				const keepIds = new Set([396, 397, 3072, 2830, 171, 172, 173, 174, 400, 402, 403, 404, 406, 622, 514, 407, 410]);
+	
+				// 处理所有可能包含 sections 的字段
+				const sectionKeys = ["ipad_recommend_sections", "ipad_more_sections", "sections_v2"];
+				for (const key of sectionKeys) {
+					if (Array.isArray(v.data[key])) {
+						v.data[key] = v.data[key].map((item) => {
+							if (item?.items?.length > 0) {
+								item.items = item.items.filter((subItem) => keepIds.has(subItem?.id));
+							}
+							return item;
+						});
+					}
+				}
+	
+				body = JSON.stringify(v);
+			} catch (error) {
+				console.log("bilibili mypage:" + error);
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/resource\/top\/activity/.test($request.url):
+			try {
+				let w = JSON.parse(body);
+				w.data && (w.data.hash = "ddgksf2013", w.data.online.icon = ""), body = JSON.stringify(w)
+			} catch (O) {
+				console.log("bilibili right corner:" + O)
+			}
+			break;
+		case /ecommerce-user\/get_shopping_info\?/.test($request.url):
+			try {
+				let P = JSON.parse(body);
+				P.data && (P.data = {
+					shopping_card_detail: {},
+					bubbles_detail: {},
+					recommend_card_detail: {},
+					selected_goods: {},
+					h5jump_popup: []
+				}), body = JSON.stringify(P)
+			} catch (W) {
+				console.log("bilibili shopping info:" + W)
+			}
+			break;
+		case /^https?:\/\/app\.bilibili\.com\/x\/v2\/search\/square/.test($request.url):
+			try {
+				let j = JSON.parse(body);
+				j.data = {
+					type: "history",
+					title: "搜索历史",
+					search_hotword_revision: 2
+				}, body = JSON.stringify(j)
+			} catch (q) {
+				console.log("bilibili hot search:" + q)
+			}
+			break;
+		case /https?:\/\/app\.bilibili\.com\/x\/v2\/account\/myinfo\?/.test($request.url):
+			try {
+				let E = JSON.parse(body);
+				E.data.vip.status || (E.data.vip.type = 2, E.data.vip.status = 1, E.data.vip.vip_pay_type = 1, E.data.vip.due_date = 466982416e4), body = JSON.stringify(E)
+			} catch (z) {
+				console.log("bilibili 1080p:" + z)
+			}
+			break;
+		case /pgc\/page\/(bangumi|cinema\/tab\?)/.test($request.url):
+			try {
+				let B = JSON.parse(body);
+				B.result.modules.forEach(t => {
+					t.style.startsWith("banner") && (t.items = t.items.filter(t => -1 != t.link.indexOf("play"))), t.style.startsWith("function") && (t.items = t.items.filter(t => -1 == t.blink.indexOf("bilibili.com")), [1283, 241, 1441, 1284].includes(t.module_id) && (t.items = [])), t.style.startsWith("tip") && (t.items = [])
+				}), body = JSON.stringify(B)
+			} catch (I) {
+				console.log("bilibili fanju:" + I)
+			}
+			break;
+		case /^https:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test($request.url):
+			try {
+				let R = JSON.parse(body);
+				if (R.data && R.data.list)
+					for (let S of R.data.list) S.duration = 0, S.begin_time = 2240150400, S.end_time = 2240150400;
+				body = JSON.stringify(R)
+			} catch (T) {
+				console.log("bilibili openad:" + T)
+			}
+			break;
+		case /^https:\/\/api\.live\.bilibili\.com\/xlive\/app-interface\/v2\/index\/feed/.test($request.url):
+			try {
+				let A = JSON.parse(body);
+				A.data && A.data.card_list && (A.data.card_list = A.data.card_list.filter(t => "banner_v1" != t.card_type)), body = JSON.stringify(A)
+			} catch (C) {
+				console.log("bilibili xlive:" + C)
+			}
+			break;
+		default:
+			$done({})
+	}
+	$done({
+		body
+	})
+} else $done({});
